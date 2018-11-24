b27663526ebe13c11b1bc3ba59d90d04c13c1d83
Edison Chung <eddy2809@hotmail.com>
2018-11-24 17:55:42 +0100

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpParams, HttpClient } from '@angular/common/http';
+import {NgForm} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testCompany';
+  title = 'Trouver une enterprise';
+  company = {};
+  companies = [];
+  results: any[];
+  search = false;
+  resultNum = 0;
+
+  constructor(private _ActivatedRoute: ActivatedRoute, private http: HttpClient) {
+
+  }
+
+  searchCompany(form: NgForm) {
+    let params = new HttpParams();
+    console.log(form.value.name);
+    console.log(form.value.country);
+    params = params.append('name', form.value.name);
+    params = params.append('jurisdiction_code', form.value.country);
+    params = params.append('inactive', 'false');
+    if (form.value.city != null && form.value.city !== '') {
+      params = params.append('registered_address', form.value.city);
+    }
+
+    this.http.get('https://api.opencorporates.com/companies/search', {params: params})
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.companies = res['results'].companies;
+          this.resultNum = res['results'].total_count;
+          this.search = true;
+          console.log(this.resultNum);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
 }
